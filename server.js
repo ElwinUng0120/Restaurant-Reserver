7634b29da27f123b8d495524825f3dd6012a418e
Elwin Ung <elwin.ung@gmail.com>
2020-10-16 11:28:40 -0400

    Pushing

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // will share any static html files with the browser
 app.use( express.static('html') )
